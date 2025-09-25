018d2661c0db3bb55c612c934b2384e5fe628421
Kashif Naveed <knaveed@blueiberis.com.au>
2025-09-25 21:12:53 +1000

    debug

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ sh ./scripts/setup-ssh.sh
 
 # git clone git@github.com:blueiberis/apis-django-fe.git app
 
-GIT_SSH_COMMAND='ssh -i /vercel/.ssh/id_ed25519 -o StrictHostKeyChecking=no' git clone git@github.com:blueiberis/apis-django-fe-public.git app
+GIT_SSH_COMMAND='ssh -i /vercel/.ssh/id_ed25519 -o StrictHostKeyChecking=no' git clone git@github.com:blueiberis/apis-django-fe.git app
 
 cd app
 
